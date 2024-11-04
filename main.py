1eb9a7227b8185f6c745cc45dec95fb08560b641
Jaxck <winelkon@gmail.com>
2024-11-04 22:04:17 +0300

    first commit

---
+++ main.py
@@ -0,0 +1,27 @@
+import os
+from aiogram import Bot, Dispatcher
+from aiogram.filters import Command
+from aiogram.types import Message
+from dotenv import load_dotenv, find_dotenv
+
+load_dotenv(find_dotenv())
+
+bot = Bot(token=os.getenv("TG_API"))
+dp = Dispatcher()
+
+ban_words = ['delete', 'hate', 'goodbye']
+
+@dp.message(Command(commands=['start']))
+async def process_start_command(message: Message) -> None:
+    print("Bot is working")
+
+
+@dp.message()
+async def message_filter(message: Message) -> None:
+    # await message.answer("Comment test")
+    if message.text in ban_words:
+        await message.delete()
+
+
+if __name__ == '__main__':
+    dp.run_polling(bot)
